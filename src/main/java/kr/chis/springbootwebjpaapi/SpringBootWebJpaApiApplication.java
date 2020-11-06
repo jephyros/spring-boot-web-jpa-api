fd819659dc102b17ec10a2bafda0097fe7722874
InseokChoi <kr.chis@gmail.com>
2020-11-06 22:59:48 +0900

    first commit

---
+++ src/main/java/kr/chis/springbootwebjpaapi/SpringBootWebJpaApiApplication.java
@@ -0,0 +1,13 @@
+package kr.chis.springbootwebjpaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebJpaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebJpaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/chis/springbootwebjpaapi/SpringBootWebJpaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.chis.springbootwebjpaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebJpaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
